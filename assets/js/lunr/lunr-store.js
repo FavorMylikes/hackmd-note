68cef5dce2cc7733d63dfc0db805c6e3e9ec73c8
FavorMylikes <FavorMylikes@users.noreply.github.com>
2021-07-29 09:04:56 +0000

    deploy: b0e1a3e78be06852ef38300163b9685d1a1095f1

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Transcription Factors & Simple Regulation",
+        "excerpt":"[HEAVILY UNFINISHED] Note: Much of this content is from Introduction to Systems Biology. This merely restates it with information I felt was lacking, left implied, and potentially unclear. This comes partially from the perspective I initially came to the subject from. Very little biology, chemistry, or even calculus knowledge, but...","categories": ["biology","systems-biology"],
+        "tags": [],
+        "url": "/hackmd-note/biology/systems-biology/2020/10/14/simple-regulation.html",
+        "teaser": null
+      }]
