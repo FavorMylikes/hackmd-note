90b16a26af283e00d14605885f516b630312b182
FavorMylikes <FavorMylikes@users.noreply.github.com>
2021-08-17 02:28:29 +0000

    deploy: a45cf9e1fd5d96748788d8472d20307da672c429

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -207,23 +207,23 @@ var store = [{
         "excerpt":"现象      贴图不完整         贴图边缘抖动      原因分析      mesh交叉         平面过薄  ","categories": ["3d"],
         "tags": [],
         "url": "/3d/Problem-Analysis-About-Texture-mapping/",
-        "teaser": "https://raw.githubusercontent.com/FavorMylikes/hackmd-note/img/img20210812183427.png"
+        "teaser": "https://raw.githubusercontent.com/FavorMylikes/hackmd-note/img/img20210812184013.png"
       },{
         "title": "Convert Camera Parameters [model of colmap]",
         "excerpt":"Try Colmap with cuda(gpu) enable gpu with args --SiftExtraction.use_gpu 1 but throw qt error, need x display close QT gui by export QT_QPA_PLATFORM=offscreen but throw these error below *** Aborted at 1628840833 (unix time) try \"date -d @1628840833\" if you are using GNU date *** PC: @ 0x7f07a36bbbf8 (unknown) ***...","categories": ["3d"],
         "tags": [],
         "url": "/3d/Convert-Camera-Parameters/",
-        "teaser": "https://raw.githubusercontent.com/FavorMylikes/hackmd-note/img/img20210812183427.png"
+        "teaser": "https://raw.githubusercontent.com/FavorMylikes/hackmd-note/img/imgogjbzom3fyingbqavx06.gif"
       },{
         "title": "PID and laplace transform",
         "excerpt":"PID Contorl 最常见的形式 $u(t)=K_pe(t)+K_i\\int_0^te(\\tau)d\\tau+K_d\\frac{d}{dt}e(t)$ 其中 $K_p$为比例(proportional)增益 $K_i$为积分(integral)增益 $K_d$为微分(derivative)增益 $e=SP-PV$为误差，设定值(setpoint)$SP$与过程值(process variable)$PV$的差 $t$为时间 $\\tau$积分变数，因为是历史状态的积分，因此要与$t$区分开来 比较常见的解释和应用 通过控制水箱的流入速度，使得水箱内的高度或体积保持不变的一种过程 汽车定速巡航，遇到上坡如何设计动力 无人机的悬停和应激相应(推一下回到原来的位置，或者前往目标点) 解决电路问题 PID 通常学习PID控制时，往往不太懂得3个变量的实际作用，又有计算机作为辅助进行模拟，那么就会导致盲目调参，而书中提到的调参方法又只讲参数含义，将调参性质描述一遍，这样完全不得法门，最后只能得到一个差不多的结果 历史上 惠更斯17世纪改造了风车，使得磨盘转速和磨盘间隙可以根据谷物数量变化 Power From the Wind , Cambridge University Press 瓦特使用圆锥摆以解决蒸汽机的速度输出 History of AutoCtrl 1868年，为了更好的保持鱼雷的深度，引入了微分控制 直到1922年，才有了完整的3项控制率 因此，大多数书籍往往会根据每项变量引入时发挥的作用加以解释，但是又与纯PI控制，PD控制，甚至单纯的P控制应用相违背，除了硬件或成本上的限制，更需要思考的是，每一项为整个系统的稳定性到底提供了多大的好处 PID调参 为了获得更好的参数，有如下几种方法 手动 Ziegler–Nichols[齐格勒－尼科尔斯方法] Ziegler–Nichols_method - Wikipedia 使用Z-N方法调节气压 - Bilibili 将ID项置0，调节P使得达到恒定震荡，再根据震荡周期和震荡值计算PID参数 注意，想要在P控制下得到稳定震荡，要有超调现象，如果通过计算机模拟，而时间单位又很小，那么无论用多大的$K_P$你都看不到超调现象，可以理解为，系统刚刚输入一点点就被感知到了，而上面视频里从气流输入到输出存在一定的延迟，因此可以使用该方法 反馈较慢 快速反馈...","categories": ["zhihu"],
         "tags": [],
         "url": "/zhihu/PID-and-laplace-transform/",
-        "teaser": "https://raw.githubusercontent.com/FavorMylikes/hackmd-note/img/img20210812183427.png"
+        "teaser": "https://raw.githubusercontent.com/FavorMylikes/hackmd-note/img/img20210816231759.png"
       },{
         "title": "Convert Camera Parameters [usage of ContextCapture Master]",
         "excerpt":"Convert parameters 拿到空三的参数文件，发现是Smart3D(soarscape.com)的软件导出的，尝试下载，发现需要申请 根据关键字查找，找到ContextCapture Master 下载安装 Model generate Add photos click submit aerotriangulation at general tab. start ContextCapture Engine wait job complish Reconstruction&gt;new Reconstruction click submit new production wait for job complish check production 3D view output Model ├── Model.mtl ├── Model.obj ├── Model_0.jpg ├── Model_1.jpg ├── Model_2.jpg └── Model_3.jpg...","categories": ["3d"],
         "tags": [],
         "url": "/3d/Camera-parameters-convert/",
-        "teaser": "https://raw.githubusercontent.com/FavorMylikes/hackmd-note/img/img20210812183427.png"
+        "teaser": "https://raw.githubusercontent.com/FavorMylikes/hackmd-note/img/img20210816194645.png"
       }]
