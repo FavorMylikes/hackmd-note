c1651be8d11d1f03d85e6ad64547d8b9bb949c9a
FavorMylikes <FavorMylikes@users.noreply.github.com>
2021-09-08 18:16:33 +0000

    deploy: 685c7311e62a94374adabea297e6af968610b74b

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,10 +1,4 @@
 var store = [{
-        "title": "Test md file",
-        "excerpt":"Test File  ","categories": ["env","system"],
-        "tags": [],
-        "url": "/env/system/test/",
-        "teaser": null
-      },{
         "title": "First Day, install ubuntu",
         "excerpt":"台式机被格式化, USB装Ubuntu系统， 18.04.5 没有登录账户 网络层的非特殊站点均要求登录 172.18.50.22:8080 目前观察到的不要求登录的站点 archive.ubuntu.com anconda.com search.cpan.org 在试图安装snap时, 出现Ca认证错误，怀疑跟网络环境有关 openssl s_client -connect api.snapcraft.io:443 出现Verification error: self signed certificate issuer=CN = H3C-HTTPS-Self-Signed-Certificate-5978b0f0a780e531 怀疑H3C路由器做了固定域名策略 装了哪些组件 apt install namp apt install traceroute apt install python apt install tree apt install conda apt install snap apt install make apt install gcc...","categories": ["env","system"],
         "tags": [],
