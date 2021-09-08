a85e1a9f32b39bbac8589fb88a2666df2f60ff2d
FavorMylikes <FavorMylikes@users.noreply.github.com>
2021-09-08 18:14:07 +0000

    deploy: 84b395b7f04c3d2f97f20c54298b14bf39366985

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "Test md file",
+        "excerpt":"Test File  ","categories": ["env","system"],
+        "tags": [],
+        "url": "/env/system/test/",
+        "teaser": null
+      },{
         "title": "First Day, install ubuntu",
         "excerpt":"台式机被格式化, USB装Ubuntu系统， 18.04.5 没有登录账户 网络层的非特殊站点均要求登录 172.18.50.22:8080 目前观察到的不要求登录的站点 archive.ubuntu.com anconda.com search.cpan.org 在试图安装snap时, 出现Ca认证错误，怀疑跟网络环境有关 openssl s_client -connect api.snapcraft.io:443 出现Verification error: self signed certificate issuer=CN = H3C-HTTPS-Self-Signed-Certificate-5978b0f0a780e531 怀疑H3C路由器做了固定域名策略 装了哪些组件 apt install namp apt install traceroute apt install python apt install tree apt install conda apt install snap apt install make apt install gcc...","categories": ["env","system"],
         "tags": [],
@@ -316,4 +322,10 @@ var store = [{
         "tags": [],
         "url": "/3d/Texture-For-Original-Model/",
         "teaser": "https://raw.githubusercontent.com/FavorMylikes/hackmd-note/img/img20210907221533.png"
+      },{
+        "title": "Texture for dog project",
+        "excerpt":"Original Texture Some sureface mesh too small be ignored. Remeshing Make mesh to Equilateral triangle 3DScan Texture after remeshing Texture after remeshing with invert matrix[0:3, 0:3] = np.linalg.inv(matrix[0:3, 0:3]) 相机不动物体动时，相机朝向需要取逆 Easy 3D Repository spark star Home Page Author Usage ------------------------------------------------------------------ F1: Help F2: Toggle animation (if available) Ctrl + 'o':...","categories": ["3d"],
+        "tags": [],
+        "url": "/3d/Texture-for-dog-project/",
+        "teaser": "https://raw.githubusercontent.com/FavorMylikes/hackmd-note/img/img20210908163126.png"
       }]
